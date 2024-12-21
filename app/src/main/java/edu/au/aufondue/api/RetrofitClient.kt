b3888fd1a6dad29879e8dddcfeab87867130ce4d
Senghurk <bin4ryhunt3r@gmail.com>
2024-12-21 21:28:17 +0700

    Working with Spring Boot Stable 1.0.1

--- app/src/main/java/edu/au/aufondue/api/RetrofitClient.kt
+++ app/src/main/java/edu/au/aufondue/api/RetrofitClient.kt
@@ -11,6 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
 class LocalDateTimeAdapter {
     @SuppressLint("NewApi")
@@ -27,7 +28,9 @@ class LocalDateTimeAdapter {
 }
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+//    private const val BASE_URL = "http://10.0.2.2:8080/"
+
+    private const val BASE_URL = "https://aufondue-backend.kindisland-399ef298.southeastasia.azurecontainerapps.io/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -35,6 +38,10 @@ object RetrofitClient {
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
+        // Add timeouts
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
         .build()
 
     private val moshi = Moshi.Builder()
